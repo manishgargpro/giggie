520172b833fc952a61d5538977df0a4923b67491
Manish Garg <manishgargpro@gmail.com>
2017-12-06 14:26:48 -0500

    changed server.js to include one script for routing to index.html

--- APIRoutes/index.js
+++ APIRoutes/index.js
@@ -2,9 +2,6 @@ const router = require("express").Router();
 const routes = require("./routes");
 
 
-// Book routes
-// router.use(isAuthenticated);
 router.use("/api", routes);
-
 
 module.exports = router;
