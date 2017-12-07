9866fc6fc6b678a7d43db6bc5fecd92a1d2df6de
Manish Garg <manishgargpro@gmail.com>
2017-12-07 18:25:45 -0500

    added homepage get route in APIRoutes/index.js

--- APIRoutes/index.js
+++ APIRoutes/index.js
@@ -4,4 +4,8 @@ const routes = require("./routes");
 
 router.use("/api", routes);
 
+router.get("/", function (req, res) {
+  res.sendFile("../client/build/index.html")
+})
+
 module.exports = router;
