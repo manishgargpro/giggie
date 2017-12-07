00a016f82c384e468f650e2d60fc044804f1af2e
Manish Garg <manishgargpro@gmail.com>
2017-12-07 18:46:56 -0500

    changed homepage route slightly

--- APIRoutes/index.js
+++ APIRoutes/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
+const path = require("path");
 const routes = require("./routes");
-
 
 router.use("/api", routes);
 
-router.get("/", function (req, res) {
-  res.sendFile("../client/build/index.html")
+router.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 })
 
 module.exports = router;
